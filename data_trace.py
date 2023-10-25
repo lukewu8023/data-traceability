8d75dfd26e381ac73115729c95d41b9a57e12c99
lukewu8023 <luke8023@gmail.com>
2023-10-25 18:42:43 +0300

    add get change logic tool

--- data_trace.py
+++ data_trace.py
@@ -1,5 +1,6 @@
 import sys
 import os
+import key_config
 
 import langchain
 from langchain_experimental.plan_and_execute import load_agent_executor
@@ -13,15 +14,12 @@ from langchain.tools import BaseTool, StructuredTool, Tool, tool
 
 import get_entity_tool
 
-os.environ["OPENAI_API_KEY"] = ""
-
-
 with open("prompt/prompt_data_trace.txt") as f:
     contents = f.read()
 
 model = ChatOpenAI(
-    openai_api_base="https://api.ohmygpt.com/v1",
     model="gpt-3.5-turbo-0613",
+    # model="gpt-4-0613",
     temperature=0,
     max_tokens=1024,
 )
@@ -49,6 +47,11 @@ tools = [
         name="Get data",
         description="get actual data in a system for data traceability application, input parameter is the system name.",
     ),
+    StructuredTool.from_function(
+        func=get_entity_tool.get_change_logic,
+        name="Get change logic",
+        description="get attribute change logic in a system for data traceability application, input parameter is the system name and user query, the return is the explanation of the logic of user asked about.",
+    ),
 ]
 # tools = load_tools(tools, llm=model)
 executor = load_agent_executor(model, tools, verbose=True)
@@ -57,5 +60,6 @@ agent = PlanAndExecute(planner=planner, executor=executor, verbose=True)
 
 langchain.debug = True
 agent.run(
-    "What is the security name of my security which id is 1234? My system name is B."
+    # "What is the security name of my security which id is 1234? My system name is B."
+    "What is the calculation logic of the value of 'gamma' attribute? My system name is B."
 )
